558dc5ad603ea205d93c19b009eb19a4b257b2e3
Karim Haddad <karim.haddad@hotmail.com>
2024-03-05 00:53:03 -0500

    Boom

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/ui_updates_demo.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const UIUpdatesDemo(),
+      ),
+    );
+  }
+}
